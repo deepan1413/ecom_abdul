6b2f69f8b1e98a3d0fc44d08e532b84d0b091678
deepan1413 <deepan14132002@gmail.com>
2026-04-04 16:16:37 +0530

    fix: env

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://souqii-one.vercel.app';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 export const API_ENDPOINTS = {
   SIGNUP: '/api/auth/signup',
